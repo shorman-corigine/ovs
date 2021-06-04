d900e7e6c4b640a1deab850f85c248313c5e5d18
Louis Peens <louis.peens@corigine.com>
2021-06-04 17:18:09 +0200

    ofproto-dpif-xlate: support meter action in a group bucket
    This patch is used to make a group bucket support a meter action.
    When receiving action=meter:<id> we need to:
    1) accept this - pre-patch effectively only set_actions are supported
    2) use this id to lookup the meter that was added
    3) populate the provider id into the ofpact_meter id.
    
    Signed-off-by: Louis Peens <louis.peens@corigine.com>
    Signed-off-by: Tianyu Yuan <tianyu.yuan@corigine.com>

--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -6570,26 +6570,7 @@ handle_flow_monitor_cancel(struct ofconn *ofconn, const struct ofp_header *oh)
     return error;
 }
 
-/* Meters implementation.
- *
- * Meter table entry, indexed by the OpenFlow meter_id.
- * 'created' is used to compute the duration for meter stats.
- * 'list rules' is needed so that we can delete the dependent rules when the
- * meter table entry is deleted.
- * 'provider_meter_id' is for the provider's private use.
- */
-struct meter {
-    struct hmap_node node;      /* In ofproto->meters. */
-    long long int created;      /* Time created. */
-    struct ovs_list rules;      /* List of "struct rule_dpif"s. */
-    uint32_t id;                /* OpenFlow meter_id. */
-    ofproto_meter_id provider_meter_id;
-    uint16_t flags;             /* Meter flags. */
-    uint16_t n_bands;           /* Number of meter bands. */
-    struct ofputil_meter_band *bands;
-};
-
-static struct meter *
+struct meter *
 ofproto_get_meter(const struct ofproto *ofproto, uint32_t meter_id)
 {
     struct meter *meter;
